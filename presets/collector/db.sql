0e983cfe35b88c784096fa38afb7be65b9eec560
nightflyza <nightfly@email.ua>
2013-08-30 16:35:30 +0300

    dbuser creation preset

---
+++ presets/collector/db.sql
@@ -0,0 +1,7 @@
+CREATE USER 'bandwidthd'@'%' IDENTIFIED BY 'dbpassword';
+
+GRANT USAGE ON * . * TO 'bandwidthd'@'%' IDENTIFIED BY 'dbpasword' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+
+CREATE DATABASE IF NOT EXISTS `bandwidthd` ;
+
+GRANT ALL PRIVILEGES ON `bandwidthd` . * TO 'bandwidthd'@'%';
